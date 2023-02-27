da8bba494839bd7362b719a6a4f57846cc231b34
Joan Rincon <jrincon@xolit.com>
2023-02-27 18:11:44 -0500

    Fin sesión 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+      
+    let number: number = 10;
+    // debugger;
+    number = number * 2;
+    number = 150;
+    number = number / 3;
+    console.log(`Number is : ${number}`);
+    }
 
 }
